0714cc7f903ec77230e48c8d3a8be95a9653bb77
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-04-11 18:35:32 -0300

    Update Atv4.c

--- lista4/Atv4.c
+++ lista4/Atv4.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-int main () {
+int main() {
     float num, raiz, cubo;
-    printf("Digite seu número: ");
-        scanf("%f", &num);
     
-    if (num > 0){
-
+    printf("Digite seu número: ");
+    scanf("%f", &num);
+    
+    if (num > 0) {
         raiz = sqrt(num); 
-        printf("A raiz do seu numero real é %.2f");}
-        
-    else if (num < 0){
+        printf("A raiz do seu número real é %.2f\n", raiz);
+    } else if (num < 0) {
         cubo = pow(num, 3);           
-        printf("O cubo de seu numero real é %2.f");}
-        
-        else{ 
-         printf("Seu numero é invalido");};
-        
-return 0;
-}    
+        printf("O cubo do seu número real é %.2f\n", cubo);
+    } else {
+        printf("Você digitou zero.\n");
+    }
+    
+    return 0;
+}
