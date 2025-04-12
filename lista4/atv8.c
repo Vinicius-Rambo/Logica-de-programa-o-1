2ba2486d108828f0193188ed5ecdb3eaec7b31f8
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-04-12 15:36:47 -0300

    Update atv8.c

--- lista4/atv8.c
+++ lista4/atv8.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
-int main () {   
+int main () {
     char gen;
     float altura, peso, imc;
-    
+
     printf("Qual seu gênero?: ");
-     scanf("%c", &gen);
+    scanf("%c", &gen);
 
     printf("Qual seu peso?: ");
-    scanf("%d", &peso);
+    scanf("%f", &peso);
 
     printf("Qual sua altura?: ");
-     scanf("%d", &altura);
+    scanf("%f", &altura);
 
-imc = peso/(altura*altura); 
+    imc = peso / (altura * altura);
 
-if(gen == 'F' || gen == 'f'){
-    if(imc < 19){
-        printf("Abaixo do peso");
-    }
-     else if(imc >= 19 && imc <= 23.9){
-         printf("Peso normal");
-     }
-    else if(imc >= 24 && imc <= 28.9){
-        printf("Obesidade leve");
-    }
-    else if(imc >= 29 && imc <=38.9){
-        printf("Obesidade moderada");
-    }
-    else if(imc >= 39){
-        printf("Obesidade mórbida");
-    }
-}
+    if(gen == 'F' || gen == 'f'){
+        if(imc < 19){
+            printf("Abaixo do peso");}
 
-else if(gen =='M' || gen == 'm'){
-     if(imc < 20){
-        printf("Abaixo do peso");}
+        else if(imc >= 19 && imc <= 23.9){
+            printf("Peso normal");}
 
-     else if(imc >= 20 && imc <= 24.9){
-         printf("Peso normal");}
+        else if(imc >= 24 && imc <= 28.9){
+            printf("Obesidade leve");}
 
-    else if(imc >= 25 && imc <= 29.9){
-        printf("Obesidade leve");}
+        else if(imc >= 29 && imc <= 38.9){
+            printf("Obesidade moderada");}
 
-    else if(imc >= 30 && imc <=39.9){
-        printf("Obesidade moderada");}
-    
+        else if(imc >= 39){
+            printf("Obesidade mórbida");}}
+
+    else if(gen == 'M' || gen == 'm'){
+        if(imc < 20){
+            printf("Abaixo do peso");}
+
+        else if(imc >= 20 && imc <= 24.9){
+            printf("Peso normal");}
+
+        else if(imc >= 25 && imc <= 29.9){
+            printf("Obesidade leve");}
+
+        else if(imc >= 30 && imc <= 39.9){
+            printf("Obesidade moderada");}
+
         else if(imc >= 40){
-        printf("Obesidade mórbida");}}
-    return 0;}      
+            printf("Obesidade mórbida");}}
+
+    return 0;
+}
