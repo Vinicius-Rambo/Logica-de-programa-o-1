0c11ca3beb39bc02444de2c1cf96025af09e46d3
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-05-08 17:30:49 -0300

    incompleto

---
+++ lista 7/atv7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char escolha;
+    int i=1, atletico = 0, coritiba = 0, foz = 0;
+    float porcentagem;
+
+do{
+    printf("- - - Qual seu time do coração aqui do paraná?\n");
+    printf("A - Atlético\n");
+    printf("C - Coritiba\n");
+    printf("F - Foz\n");
+    printf("S - Sair\n");
+
+    printf("Escolha uma das opções: ");
+    scanf(" %c", escolha);
+
+    
+
+
+    
+} while (escolha == 'S');
+    
