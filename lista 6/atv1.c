0072f333826646b41035a1f7a27ef8454f5b44d3
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-05-15 19:15:15 -0300

    a

---
+++ lista 6/atv1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){   
+    int i=1;
+    printf("Contador de 1 a 20\n");
+
+    while(i <= 20){
+        printf("%d\n", i);
+        i++;
+    }
+return 0;}
---
+++ lista 6/atv2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){   
+    int i=15;
+    printf("Contador de 15 para 0\n");
+
+    while(i >= 0){
+        printf("%d\n", i);
+        i--;}
+return 0;}
