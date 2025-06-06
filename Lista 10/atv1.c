e4d49c9e3f1700d612efcc80f123a03c0f5a7f46
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-06-06 10:37:17 -0300

    Update atv1.c

--- Lista 10/atv1.c
+++ Lista 10/atv1.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 int main(){
-  int matriz [8][8], i, j;
+  int matriz[8][8], i, j;
   int maior, menor;
 
   for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
-
-    printf("Digite o valor da matriz [%d] [%d]:", i, j );
-    scanf("%d", &matriz [i][j]);
+      printf("Digite o valor da matriz [%d] [%d]:", i, j );
+      scanf("%d", &matriz[i][j]);
   }
 
-maior = matriz[0][0];
-menor = matriz[0][0]; 
+  maior = matriz[0][0];
+  menor = matriz[0][0]; 
 
- for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
-
-   if(matriz[i][j] > maior) {
-        maior = matriz[i][j] ;
-   }
-   if(matriz[i][j] < menor) {
-        menor = matriz[i][j] ;
-   }
-
-
-printf("\nmaior valor: %d\n", maior);
-    for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
-        if(matriz[i][j] == maior) {
-             printf("\t\t linha:[%.2d]\n", i);
-             printf("\t\t coluna:[%.2d]\n", j);   
-        }    
+  for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
+      if(matriz[i][j] > maior) {
+        maior = matriz[i][j];
       }
-
-printf("\nmenor valor: %d\n", menor);   
-    for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
-        if(matriz[i][j] == menor) {
-             printf("\t\t linha:[%.2d]\n", i);
-             printf("\t\t coluna:[%.2d]\n", j);   
-        }    
+    
+      if(matriz[i][j] < menor) {
+        menor = matriz[i][j];
       }
-return 0;}
+  }
+
+  printf("\nmaior valor: %d\n", maior);
+  for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
+      if(matriz[i][j] == maior) {
+        printf("\t\t linha:[%.2d]\n", i);
+        printf("\t\t coluna:[%.2d]\n", j);   
+      }    
+  }
+
+  printf("\nmenor valor: %d\n", menor);   
+  for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
+      if(matriz[i][j] == menor) {
+        printf("\t\t linha:[%.2d]\n", i);
+        printf("\t\t coluna:[%.2d]\n", j);   
+      }    
+  }
+
+  return 0;
+}
