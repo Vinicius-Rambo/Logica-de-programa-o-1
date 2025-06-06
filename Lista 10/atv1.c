a87508f8b9a33bea39d4bef016f5c9df3059aff7
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-06-06 10:30:17 -0300

    Update atv1.c

--- Lista 10/atv1.c
+++ Lista 10/atv1.c
@@ -3,8 +3,38 @@ int main(){
   int matriz [8][8], i, j;
   int maior, menor;
 
-  for(i = 0; i < 8 ; i++){for (j = 0; j < 8 ; j++){
+  for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
 
     prinft("Digite o valor da matriz [%d] [%d]", i, j );
     scanf("%d", &matriz [i][j]);
   }
+
+maior = matriz[0][0];
+menor = matriz[0][0]; 
+
+ for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
+
+   if(matriz[i][j] > maior) {
+        maior = matriz[i][j] ;
+   }
+   if(matriz[i][j] > menor) {
+        maior = matriz[i][j] ;
+   }
+
+
+printf("\nmaior valor: %d\n", maior);
+    for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
+        if(matriz[i][j] == maior) {
+             printf("\t\t linha:[%.2d]\n", i);
+             printf("\t\t coluna:[%.2d]\n", j);   
+        }    
+      }
+
+printf("\nmenor valor: %d\n", menor);   
+    for(i = 0; i < 8 ; i++) for(j = 0; j < 8 ; j++){
+        if(matriz[i][j] == menor) {
+             printf("\t\t linha:[%.2d]\n", i);
+             printf("\t\t coluna:[%.2d]\n", j);   
+        }    
+      }
+return 0;}
