53392c6da6c734227acc53e591cb83967c266f64
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-06-18 16:32:24 -0300

    Create atv.c

---
+++ Lista 12/atv.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <string.h>
+#include<ctype.h>
+
+#define TAM 100
+
+int main(){
+struct Tpessoa{
+    int codigo;
+    char nome[100];
+    char genero;
+    int idade;
+    float altura;
+    float peso;
+};
+
+int n, i;
+struct Tpessoa cadastro[n];
+
+
+
+
+    
+
+
+
+return 0; }
