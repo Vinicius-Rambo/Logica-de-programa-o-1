dc0b9ee1bc45047a8c9783e6ad72676adc102177
Vinicius-Rambo <vncs.rambo@gmail.com>
2025-06-23 08:10:11 -0300

    Update atv.c

--- Lista 11 - Struct/atv.c
+++ Lista 11 - Struct/atv.c
@@ -2,10 +2,24 @@
 #include <string.h>
 #include<ctype.h>
 
-#define TAM 8
-
+#define TAM 100
 
 int main(){
+struct Tpessoa{
+    int codigo;
+    char nome[100];
+    char genero;
+    int idade;
+    float altura;
+    float peso;
+};
+
+int n, i;
+struct Tpessoa cadastro[n];
+
+
+
+
     
 
 
